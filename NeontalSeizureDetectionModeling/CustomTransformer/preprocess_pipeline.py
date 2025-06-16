b968ffa1d667006f0dd056042ea40c46edca0258
Geletaw Sahle <gel1has3@users.noreply.github.com>
2025-06-16 15:53:18 +0100

    Create preprocess_pipeline.py

---
+++ NeontalSeizureDetectionModeling/CustomTransformer/preprocess_pipeline.py
@@ -0,0 +1,16 @@
+# preprocess_pipeline.py
+
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+class EEGDatasetFromNumpy(Dataset):
+    def __init__(self, data, labels):
+        self.data = torch.tensor(data, dtype=torch.float32)
+        self.labels = torch.tensor(labels, dtype=torch.long)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx], self.labels[idx]
